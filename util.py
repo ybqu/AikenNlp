330f4b241afd79fe1b82c1ea13e08a4feb20d320
yuanbinqu <32428962+quxiansen@users.noreply.github.com>
2020-04-23 14:03:55 +0800

    :wrench: Add util file

---
+++ util.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   util.py
+@Time    :   2020/04/23 10:48:32
+@Author  :   Aiken 
+@Version :   1.0
+@Contact :   2191002033@cnu.edu.cn
+@License :   
+@Desc    :   工具类
+'''
+
+# here put the import lib
+class Util():
+    @staticmethod
+    def returnData(code, msg, count, data):
+        return {"code": code,  "msg": msg, "count": count,"data": data}
