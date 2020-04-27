1ee78a3a7beb823b28dce8389bb51e709e47f737
yuanbinqu <32428962+quxiansen@users.noreply.github.com>
2020-04-27 13:14:56 +0800

    :zap: Define global variables

--- apps/metaphor/models.py
+++ apps/metaphor/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from .MetaphorModel import main
+from .MetaphorModel.main import METAPHOR_INTERPRET, METAPHOR_GENERATE
 
 # Create your models here.
 class Interpretation(models.Model):
     @staticmethod
     def interprete(target, source):
-        attribution, interpretation = main.metaphor_interpret(target, source)
+        attribution, interpretation = METAPHOR_INTERPRET.metaphor_interpret(target, source)
         data_list = []
         for p, s in zip(attribution, interpretation):
             data = {'target': target, 'source': source, 'attribution': p, 'interpretation': s}
@@ -15,7 +15,7 @@ class Interpretation(models.Model):
 class Generation(models.Model):
     @staticmethod
     def generation(target, attribution):
-        sources, sentences = main.metaphor_generate(target, attribution)
+        sources, sentences = METAPHOR_GENERATE.metaphor_generate(target, attribution)
 
         data_list = []
         for s, sent in zip(sources, sentences):
