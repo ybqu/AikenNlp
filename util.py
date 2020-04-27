e8714af7f444128454d94e83d157c11274925578
yuanbinqu <32428962+quxiansen@users.noreply.github.com>
2020-04-28 01:14:19 +0800

    :wrench: Add new util function

--- util.py
+++ util.py
@@ -18,3 +18,21 @@ class Util():
         ? 定义后端返回数据格式 
         """
         return {"code": code,  "msg": msg, "count": count,"data": data}
+
+    @staticmethod
+    def get_metaphor_labels():
+        """ 
+        ? 获取隐喻 label
+        """
+        return ['PAD', 'CLS', 'SEP', "START", "STOP", "O", "ss", "sb", "si", "se", "ts", "tb", "ti", "te", "aps", "apb", "api", "ape", "vps", "vpb", "vpe", "vpi"]
+
+    @staticmethod
+    def metaphor_label():
+        """ 
+        ? 获取隐喻 label 相关数据
+        """
+        label_list = Util.get_metaphor_labels()
+
+        label_map = {label: i for i, label in enumerate(label_list)}
+        num_labels = len(label_list)
+        return num_labels, label_map
